1b8ec125b42fedc9784cd9e5b3e06388eef9baab
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-11-12 18:01:19 +0300

    12.11.2021-18:01

--- pages/cart.js
+++ pages/cart.js
@@ -1,7 +1,18 @@
+import Head from "next/head"
+import { useContext } from "react"
+import {DataContext} from "../store/GlobalState"
 const Cart=()=>{
+
+    const {state,dispatch}=useContext(DataContext)
+
+    const {cart}=state
+    if(cart.length===0) return <img className="img-responsive w-100" src="/empty-cart.jpg" alt="empty-cart"/>
     return(
         <div>
-            Cart
+           <Head>
+               <title>Cart Page</title>
+           </Head>
+           <h1>Cart</h1>
         </div>
     )
 }
